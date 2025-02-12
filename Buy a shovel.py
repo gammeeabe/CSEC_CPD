b5a743c33214833845cba89a01538ab9cd21167c
gammeeabe <gammee1998@gmail.com>
2025-02-12 12:00:01 -0800

    Create Buy a shovel.py

---
+++ Buy a shovel.py
@@ -0,0 +1,9 @@
+# Read input values
+k, r = map(int, input().split())
+
+# Try increasing n until a valid condition is met
+for n in range(1, 11):  # At most, we need to check up to 10
+    total_price = n * k
+    if total_price % 10 == 0 or total_price % 10 == r:
+        print(n)
+        break
